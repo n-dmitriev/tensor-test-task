6b967ae36b0019a38b173faded04bd23e7f738e1
Никита <nikita-dmitr@yandex.ru>
2020-03-10 13:06:28 +0300

    Создание ветки для внедрения redux-а

--- src/index.js
+++ src/index.js
@@ -3,11 +3,30 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import {BrowserRouter} from 'react-router-dom'
+import {applyMiddleware, compose, createStore} from 'redux'
+import rootReducer from './store/reducers/rootReducer'
+import thunk from 'redux-thunk'
+import {Provider} from 'react-redux'
+
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        }) : compose;
+
+const store = createStore(
+    rootReducer,
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
+)
 
 const app = (
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 )
 
 ReactDOM.render(app, document.getElementById('root'))
